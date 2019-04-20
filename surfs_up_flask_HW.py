bbaa0e0ded2c974918e8079e2cd8825dc9744fd0
Lori B <47092293+loribeth18@users.noreply.github.com>
2019-04-19 20:47:46 -0500

    Add files via upload
    upload changes

--- surfs_up_flask_HW.py
+++ surfs_up_flask_HW.py
@@ -1,13 +1,32 @@
 # -*- coding: utf-8 -*-
 """
-Created on Fri Apr 19 10:17:51 2019
+Created on Fri Apr 19 19:49:37 2019
 
 @author: lorib
 """
 
-
 # 1. import Flask
-from flask import Flask
+from flask import Flask, jsonify
+
+import datetime as dt
+import numpy as np
+import pandas as pd
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+engine = create_engine("sqlite:///hawaii.sqlite")
+
+Base = automap_base()
+Base.prepare(engine, reflect=True)
+
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
+session = Session(engine)
+
 
 # 2. Create an app, being sure to pass __name__
 app = Flask(__name__)
@@ -30,29 +49,42 @@ def home():
 # 4. Define what to do when a user hits the /about route
 @app.route("/api/v1.0/precipitation")
 def precipitation():
-    print("Server received request for 'precipitation' page...")
-    return "Welcome to my 'About' page!"
+   precipitation = session.query(Measurement.date, Measurement.prcp).\
+    order_by(Measurement.date).all()
+   d = {date: prcp for (date, prcp) in precipitation}
+   return jsonify(d)
 
 
-    return jsonify(precipitation)
+@app.route("/api/v1.0/stations")
+def stations():
+   active_stations=session.query(Station.station, Station.name).\
+    order_by(Station.station).desc().all()
+   s = {station: name for (station, name) in active_stations}
+   return jsonify(s)
 
-Convert the query results to a Dictionary using date as the key and prcp as the value.
-Return the JSON representation of your dictionary.
+@app.route("/api/v1.0/tobs")
+def tobs():
+   tob = session.query(Measurement.date, Measurement.prcp).\
+    filter(Measurement.date >= '2016-08-23').\
+    order_by(Measurement.date).all()
+   t = {date: prcp for (date, prcp) in tob}
+   return jsonify(t)
 
-@app.route("/api/v1.0/justice-league/superhero/<superhero>")
-def justice_league_character(superhero):
-    """Fetch the Justice League character whose real_name matches
-       the path variable supplied by the user, or a 404 if not."""
 
-    canonicalized = superhero.replace(" ", "").lower()
-    for character in justice_league_members:
-        search_term = character["superhero"].replace(" ", "").lower()
+@app.route("/api/v1.0/<start>")
+def start(start_date):
+    canonicalized = date.replace(" ", "").lower()
+    return session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
+        filter(Measurement.date >= start_date).all()
 
-        if search_term == canonicalized:
-            return jsonify(character)
 
-    return jsonify({"error": f"Character with superhero {superhero} not found."}), 404
 
+@app.route("/api/v1.0/<start>/<end>")
+def trip_calc_temps(start_date, end_date):
+    return session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
+        filter(Measurement.date >= start_date).filter(Measurement.date <= end_date).all()
+        
+        
 
 if __name__ == "__main__":
     app.run(debug=False)
